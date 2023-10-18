19ec371151e479a301633d98db40e9b7553cf9d7
Mohibullah Jameel <mohibullah.bellmedex@gmail.com>
2023-10-18 09:29:58 -0700

    dynamic params

--- app/page4/[post]/page.tsx
+++ app/page4/[post]/page.tsx
@@ -1,27 +1,35 @@
 // generation of all paths statically at build time
 
+
+// use this to rendered 404 if the path is not in the ones generated at the build time
+const dynamicParams = false;
+export { dynamicParams };
+
+
 export async function generateStaticParams() {
   // it will statically generate all the pages
 
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
   const posts = await res.json();
 
+  /*  by default it will dynamically generate the page on request if the page is not statically build at build time, 
+  e.g if it has 100 pages if we go to 101 page it will not through 404 insted it will generate the page on request */
   //   if you want to build only certain pages of the routes then return an array with them e.g
-  return [
-    { post: "1" },
-    { post: "2" },
-    { post: "3" },
-    { post: "4" },
-    { post: "5" },
-    { post: "6" },
-  ]; //it will generate only 4 pages.
+  // return [
+  //   { post: "1" },
+  //   { post: "2" },
+  //   { post: "3" },
+  //   { post: "4" },
+  //   { post: "5" },
+  //   { post: "6" },
+  // ]; //it will generate only 4 pages.
 
   // name should be same as slug
 
   //build all routes
-  // return posts.map((post: any) => ({
-  //   post: post.id.toString(),
-  // }));
+  return posts.map((post: any) => ({
+    post: post.id.toString(),
+  }));
 }
 
 const getPost = async (id: string) => {
